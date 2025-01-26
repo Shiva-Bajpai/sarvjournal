d76cb6d8e432e60c7672c3b4c841af9c0cae1c84
Shiva Bajpai <shivabajpai2006@gmail.com>
2025-01-26 15:30:00 +0530

    feat: add style utilities - animations, skeleton, spacing

---
+++ src/styles/index.js
@@ -0,0 +1,7 @@
+// Styles barrel exports - imports all global stylesheets
+// Styles barrel export
+import './animations.css';
+import './skeleton.css';
+import './spacing.css';
+import './toast.css';
+import './desktop.css';
